b5692a03b62bd0230541ba12b819eeae2d78787d
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2021-01-21 11:48:33 +0300

    export script

---
+++ src/routes/team.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+    <title>About</title>
+</svelte:head>
+
+
+<div class="flex items-center justify-center flex-1">
+    <div>
+        <h1>About this site</h1>
+        <p>This is the 'about' page. There's not much here.</p>
+    </div>
+</div>
