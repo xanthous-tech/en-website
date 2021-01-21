bcbc8f8e14061bcdedd02c2252383b412b27cf0e
Osman Mesut Ozcan <osmanmesutozcan@gmail.com>
2021-01-21 16:37:28 +0300

    add meta description

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-    import Nav from '../components/Nav.svelte';
-    import Footer from '../components/Footer.svelte';
+    import Nav from "../components/Nav.svelte";
+    import Footer from "../components/Footer.svelte";
 
     export let segment: string;
 </script>
+
+<svelte:head>
+    <title>X-Tech</title>
+    <meta name="We're a software development team with experience in building products for companies of all sizes." />
+</svelte:head>
+
+<div class="flex flex-col min-h-screen bg-gray-200">
+    <Nav {segment} />
+
+    <main class="flex flex-col flex-1">
+        <slot />
+    </main>
+
+    <Footer />
+</div>
 
 <style global lang="postcss">
     @tailwind base;
@@ -16,17 +31,3 @@
         height: 100vh;
     }
 </style>
-
-<svelte:head>
-    <title>X-Tech</title>
-</svelte:head>
-
-<div class="flex flex-col min-h-screen bg-gray-200">
-    <Nav {segment} />
-
-    <main class="flex flex-col flex-1">
-        <slot></slot>
-    </main>
-
-    <Footer />
-</div>
